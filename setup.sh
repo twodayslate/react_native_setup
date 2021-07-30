6a2d982c37333a9db60a01a7c914e7102c90cfff
twodayslate <zac@gorak.us>
2021-07-29 22:53:24 -0400

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+set -x
+set -e
 
 brew install node
 brew install watchman
 sudo gem install cocoapods
 
 # https://github.com/CocoaPods/CocoaPods/issues/10220#issuecomment-730963835
-sudo arch -x86_64 gem install ffi
+sudo gem install ffi
 
 npx react-native init AwesomeProject
 
 cd AwesomeProject
 
 pushd ios
-#pod install
-arch -x86_64 pod install
+# https://stackoverflow.com/a/68543052/193772
+rm -rf ~/.cocoapods/repos/cocoapods
+pod install
 popd
 
 # npx react-native start &
